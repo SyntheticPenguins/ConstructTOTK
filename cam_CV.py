f7bf5c208c037bf629ac0171bd9b222936b42e29
Synthetic Penguins <synthetic.penguins@gmail.com>
2023-11-12 01:38:53 -0500

    created cam

--- cam_CV.py
+++ cam_CV.py
@@ -1,24 +1,32 @@
 import cv2
-
-# webcam
-camera = cv2.VideoCapture(0)
-camera.set(3, 640)
-camera.set(4, 480)
-
-# face cascade
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# main loop
-while True:
-    success, img = camera.read()
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+import os
     
-    
-    faces = faceCascade.detectMultiScale(imgGray)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(img, "TOILET", (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 1)
-    
-    cv2.imshow("Output", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break 
+class Camera:
+    def __init__(self):
+        self.camera = cv2.VideoCapture(0)
+        self.camera.set(3, 640)
+        
+        frame_num = int(self.camera.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video_writer = cv2.VideoWriter(f"Videos//Video {len(os.listdir('//home//pi//SwordBot//src//Videos')) + 1}.mp4",fourcc, 25.0,(frame_width,frame_height))
+        
+        self.faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    def tick(self):
+        # called every tick
+        success, img = self.camera.read()
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        faces = self.faceCascade.detectMultiScale(imgGray)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(img, "TARGET", (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 1)
+            
+        self.video_writer.write(img)
+        return img, faces
+    def kill(self):
+        self.camera.release()
+        self.video_writer.release()
+        del self
